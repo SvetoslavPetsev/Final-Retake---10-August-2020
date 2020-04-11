1bb07373688a5b4892d52a573de7d4de111ae243
Svetoslav Petsev <svetoslav_petsev@users.noreply.github.com>
2020-04-11 07:06:53 +0300

    Add project files.

---
+++ Need for Speed III/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Need_for_Speed_III
+{
+    public class Status
+    {
+        public int Mileage { get; set; }
+        public int Fuel { get; set; }
+        public Status(int mileage, int fuel)
+        {
+            this.Mileage = mileage;
+            this.Fuel = fuel;
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+
+            Dictionary<string, Status> carList = new Dictionary<string, Status>();
+
+            int numberCars = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberCars; i++)
+            {
+                string[] newCar = Console.ReadLine().Split("|");
+                string name = newCar[0];
+                int mileage = int.Parse(newCar[1]);
+                int fuel = int.Parse(newCar[2]);
+                Status newStatus = new Status(mileage, fuel);
+                carList.Add(name, newStatus);
+            }
+
+            string input;
+            while ((input = Console.ReadLine()) != "Stop")
+            {
+                string[] info = input.Split(" : ");
+                string cmd = info[0];
+
+                if (cmd == "Drive")
+                {
+                    string name = info[1];
+                    int distance = int.Parse(info[2]);
+                    int fuel = int.Parse(info[3]);
+
+                    if (carList.ContainsKey(name))
+                    {
+                        if (carList[name].Fuel >= fuel)
+                        {
+                            carList[name].Fuel -= fuel;
+                            carList[name].Mileage += distance;
+
+                            Console.WriteLine($"{name} driven for {distance} kilometers. {fuel} liters of fuel consumed.");
+
+                            if (carList[name].Mileage >= 100000)
+                            {
+                                carList.Remove(name);
+                                Console.WriteLine($"Time to sell the {name}!");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not enough fuel to make that ride");
+                        }
+                    }
+                }
+                else if (cmd == "Refuel") //problem?
+                {
+                    string name = info[1];
+                    int fuel = int.Parse(info[2]);
+
+                    if (carList.ContainsKey(name))
+                    {
+                        if (carList[name].Fuel + fuel > 75)
+                        {
+                            fuel = 75 - carList[name].Fuel;
+                        }
+                        else if (carList[name].Fuel == 75)
+                        {
+                            fuel = 0;
+                        }
+                        carList[name].Fuel += fuel;
+                        Console.WriteLine($"{name} refueled with {fuel} liters");
+                    }
+                }
+                else if (cmd == "Revert")
+                {
+                    string name = info[1];
+                    int kilometers = int.Parse(info[2]);
+
+                    if (carList.ContainsKey(name))
+                    {
+                        carList[name].Mileage -= kilometers;
+                        if (carList[name].Mileage < 10000)
+                        {
+                            carList[name].Mileage = 10000;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{name} mileage decreased by {kilometers} kilometers");
+                        }
+                    }
+                }
+            }
+
+            foreach (var car in carList.OrderByDescending(x => x.Value.Mileage).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{car.Key} -> Mileage: {car.Value.Mileage} kms, Fuel in the tank: {car.Value.Fuel} lt.");
+            }
+        }
+    }
+}
---
+++ SecretChat/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+
+namespace SecretChat
+{
+    class Program
+    {
+        static void Main()
+        {
+            string text = Console.ReadLine();
+            string input;
+            while ((input = Console.ReadLine())!= "Reveal")
+            {
+                string[] info = input.Split(":|:");
+                string cmd = info[0];
+                if (cmd == "InsertSpace")
+                {
+                    int index = int.Parse(info[1]);
+                    text = text.Insert(index, " ");
+                    Console.WriteLine(text);
+                }
+                else if (cmd == "Reverse")
+                {
+                    string substr = info[1];
+                    if (text.Contains(substr))
+                    {
+                        int index = text.IndexOf(substr);
+                        text = text.Remove(index, substr.Length);
+                        char[] substrArr = substr.ToCharArray();
+                        Array.Reverse(substrArr);
+                        string reversed = new string(substrArr);
+                        text = text.Insert(text.Length, reversed);
+                        Console.WriteLine(text);
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                }
+                else if (cmd == "ChangeAll")
+                {
+                    string substr = info[1];
+                    string replacement = info[2];
+
+                    if (text.Contains(substr))
+                    {
+                        text = text.Replace(substr, replacement);
+                        Console.WriteLine(text);
+                    }
+                }
+            }
+            Console.WriteLine($"You have a new text message: {text}");
+        }
+    }
+}
